6e259e8e83df505edf4336b518c2e726b9a3c322
Gian Lucca Ricupito Dos Santos <gian.santos@saraiva.com.br>
2019-02-07 11:59:43 -0200

    Updates

---
+++ precache-manifest.081583bbfc97d10e53d296cbb0075c70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1786b4060c28a66e65e",
+    "url": "/calculadora_react/static/js/runtime~main.f1786b40.js"
+  },
+  {
+    "revision": "d77a54c0f4b4f66d9413",
+    "url": "/calculadora_react/static/js/main.d77a54c0.chunk.js"
+  },
+  {
+    "revision": "deb620844dea81b3de13",
+    "url": "/calculadora_react/static/js/1.deb62084.chunk.js"
+  },
+  {
+    "revision": "d77a54c0f4b4f66d9413",
+    "url": "/calculadora_react/static/css/main.f7ba0868.chunk.css"
+  },
+  {
+    "revision": "bc2bdc03d9d36b46bed01c5d5810e668",
+    "url": "/calculadora_react/index.html"
+  }
+];
